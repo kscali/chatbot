f02c33552810caf9f6b237e6e39c662d914a5031
Koychian Saeteurn <kosatun@aol.com>
2018-11-15 11:58:40 -0800

    added a response to chatbot

--- irc.rb
+++ irc.rb
@@ -11,12 +11,18 @@ socket.puts "NICK #{nickname}"
 socket.puts "USER #{nickname} 0 * #{nickname}"
 socket.puts "JOIN #{channel}"
  
+socket.puts "PRIVMSG #{channel} :I am so happy to be here!"
+ 
 while message = socket.gets do
   puts message
-  
+ 
   if message.match('^PING :')
-    server = message.split(":").last 
+    server = message.split(':').last
     puts "PONG #{server}"
     socket.puts "PONG #{server}"
-  end  
+  elsif message.match('How are you?')
+    puts "PRIVMSG #{channel} :I'm great, thanks!"
+    socket.puts "PRIVMSG #{channel} :I'm great, thanks!"
+    
+  end
 end
