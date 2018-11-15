a2b92ddd2e2a47e9b1c82c4c1e00de5567726969
Koychian Saeteurn <kosatun@aol.com>
2018-11-15 11:19:31 -0800

    added a pong to the ping and added self to chatroom.

--- irc.rb
+++ irc.rb
@@ -5,10 +5,18 @@ port    = 6667
 socket  = TCPSocket.open(server, port)
  
 nickname = 'SkillcrushBotOMG'
+channel = '#CodersAlive83'
  
 socket.puts "NICK #{nickname}"
 socket.puts "USER #{nickname} 0 * #{nickname}"
+socket.puts "JOIN #{channel}"
  
 while message = socket.gets do
   puts message
+  
+  if message.match('^PING :')
+    server = message.split(":").last 
+    puts "PONG #{server}"
+    socket.puts "PONG #{server}"
+  end  
 end
